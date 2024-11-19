b5d84b2fde99da3efdea56eb9973732342d5a272
Matheus Marchon <143226443+Marchon1702@users.noreply.github.com>
2024-11-19 20:52:07 -0300

    Adicionando minha praticas de lógica

---
+++ praticas-c-sharp-codes/lista-de-linguagens/Program.cs
@@ -0,0 +1,6 @@
+﻿// Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
+// Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
+
+string[] linguagens = ["Java", "C#", "JavaScript"];
+
+Console.WriteLine(linguagens[1]);
