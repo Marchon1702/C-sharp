76f5e3704f98bed0914127c646dff59331ecc2e3
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-20 18:51:54 -0300

    Conclui exercicio de criação de login

---
+++ login-c#/Program.cs
@@ -0,0 +1,39 @@
+﻿// Criar um programa que simule um sistema de login utilizando um dicionário
+// para armazenar nomes de usuário e senhas.
+
+Dictionary<string, string> userAndPassword = new Dictionary<string, string>
+{
+    { "Matheus", "matheus2000"}
+};
+
+Console.WriteLine("*****************");
+Console.WriteLine("BarberShopManager");
+Console.WriteLine("*****************\n");
+
+Console.Write("Digite seu nome de usuário: ");
+string username = Console.ReadLine()!;
+
+if(userAndPassword.ContainsKey(username))
+{
+    Console.Write("Digite sua senha: ");
+    string senhaDigitada = Console.ReadLine()!;
+
+    if (senhaDigitada == userAndPassword[username])
+    {
+        Console.WriteLine($"Seja Bem vindo {username}");
+    } else
+    {
+        Console.WriteLine("Senha incorreta, tente novamente mais tarde!");
+    }
+
+} else
+{
+    Console.WriteLine("Usuário não encontrado!");
+}
+
+   
+
+
+
+
+
