47cb2f242f7571bdea415681f22f7d7c6810352d
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-20 18:04:14 -0300

    Conclui Exercicio nota média usando Dinicionários.

--- notas-media-com-dicionario/Program.cs
+++ notas-media-com-dicionario/Program.cs
@@ -1,4 +1,21 @@
 ﻿// Criar um dicionário que represente um aluno,
 // com uma lista de notas, e mostre a média de suas notas na tela.
 
-Console.WriteLine("alterado");
+Dictionary<string, List<int>> notasDoAluno = new Dictionary<string, List<int>>
+{
+    { "Matheus", new List<int> {8, 9, 5} }
+};
+
+void MostrarMedia()
+{
+    foreach (string aluno in notasDoAluno.Keys)
+    {
+        List<int> notas = notasDoAluno[aluno];
+        double notaMedia = notas.Average();
+        Console.WriteLine($"A nota média de {aluno} é {notaMedia}");
+        Thread.Sleep(5000);
+    }
+}
+
+MostrarMedia();
+
