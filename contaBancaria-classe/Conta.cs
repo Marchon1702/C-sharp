7a400e200bcead9da7d1e734d5bd61063a16d462
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-21 19:31:41 -0300

    Exercicio da aula 1 c# orientado a objetos

---
+++ contaBancaria-classe/Conta.cs
@@ -0,0 +1,18 @@
+﻿//Criar uma classe que representa uma conta bancária, com um número indicador, titular, saldo e senha.
+//Desenvolver um método da classe Conta que exibe suas informações.
+
+class Conta
+{
+    public int numeroDaConta;
+    public string nomeDoTitular;
+    public int senha;
+
+    public void mostrarDadosConta()
+    {
+        Console.WriteLine($@"
+Conta: {numeroDaConta}
+Titular: {nomeDoTitular}
+Senha: {senha}
+        ");
+    }
+}
---
+++ contaBancaria-classe/Program.cs
@@ -0,0 +1,9 @@
+﻿//Criar um objeto do tipo Conta e adicionar dados.
+
+Conta conta1 = new Conta();
+conta1.numeroDaConta = 1790029;
+conta1.nomeDoTitular = "Rufino";
+conta1.senha = 400289;
+
+conta1.mostrarDadosConta();
+
