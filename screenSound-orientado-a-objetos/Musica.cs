8612ecbfacb7e169a60b5bfddeab68a0a4d707aa
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-21 20:59:29 -0300

    Adicionando conteúdos da aula 3, c# orientado a objetos.

--- screenSound-orientado-a-objetos/Musica.cs
+++ screenSound-orientado-a-objetos/Musica.cs
@@ -1,21 +1,24 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
-    public bool disponivel;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string Descricao => $"A música {Nome} pertence a banda {Artista}.";
 
     public void ExibirInfosDaMusica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração {duracao} seg");
-        if(disponivel)
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração {Duracao} seg");
+        Console.WriteLine($"Descrição: {Descricao}");
+        if(Disponivel)
         {
             Console.WriteLine("Ouça agora!");
         } else
         {
             Console.WriteLine("Assine o plano Plus+");
         }
+        Console.WriteLine("--------------------------------");
     }
 }
--- screenSound-orientado-a-objetos/Program.cs
+++ screenSound-orientado-a-objetos/Program.cs
@@ -1,16 +1,17 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Faroeste Caboclo";
-musica1.artista = "Legião Urbana";
-musica1.duracao = (9 * 60);
-musica1.disponivel = true;
+musica1.Nome = "Faroeste Caboclo";
+musica1.Artista = "Legião Urbana";
+musica1.Duracao = (9 * 60);
+musica1.Disponivel = true;
 
 musica1.ExibirInfosDaMusica();
 
 Musica musica2 = new Musica();
-musica2.nome = "Sweet child on mine";
-musica2.artista = "Guns and Roses";
-musica2.duracao = 303; 
-musica2.disponivel = false;
+musica2.Nome = "Sweet child on mine";
+musica2.Artista = "Guns and Roses";
+musica2.Duracao = 303; 
+musica2.Disponivel = false;
 
 musica2.ExibirInfosDaMusica();
 
+Thread.Sleep(10000);
