91cdf6b7d242d614e83ad6dfa49f7783a71eb342
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-21 19:07:03 -0300

    Aula-1 do curso c# orientado a objetos

---
+++ screenSound-orientado-a-objetos/Musica.cs
@@ -0,0 +1,21 @@
+﻿class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirInfosDaMusica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração {duracao} seg");
+        if(disponivel)
+        {
+            Console.WriteLine("Ouça agora!");
+        } else
+        {
+            Console.WriteLine("Assine o plano Plus+");
+        }
+    }
+}
---
+++ screenSound-orientado-a-objetos/Program.cs
@@ -0,0 +1,16 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Faroeste Caboclo";
+musica1.artista = "Legião Urbana";
+musica1.duracao = (9 * 60);
+musica1.disponivel = true;
+
+musica1.ExibirInfosDaMusica();
+
+Musica musica2 = new Musica();
+musica2.nome = "Sweet child on mine";
+musica2.artista = "Guns and Roses";
+musica2.duracao = 303; 
+musica2.disponivel = false;
+
+musica2.ExibirInfosDaMusica();
+
