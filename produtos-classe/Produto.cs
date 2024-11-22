5cb8c9e2ba1ed41b1a42d5ecebca68d926808bd1
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-21 21:26:35 -0300

    Exercicio aula 3, criando e gerenciando a classe Produto.

---
+++ produtos-classe/Produto.cs
@@ -0,0 +1,42 @@
+﻿// Desenvolver a classe Produto, com os atributos nome, marca, preco e estoque. Além disso, garantir que o preço e o estoque do produto sejam valores positivos e criar uma propriedade que mostra detalhadamente as informações do produto, para que seja usado pela equipe de vendas.
+
+class Produto
+{
+    public string nome;
+    public string marca;
+    private int preco;
+    private int estoque;
+    public string DetalhesProduto => 
+        $@"
+----{nome}-{marca}----
+Preço: {preco}
+Estoque: {estoque}
+----------------------
+";
+
+    public void AdicionaPreco(int preco)
+    {
+        if(preco >= 0)
+        {
+            this.preco = preco;
+            Console.WriteLine($"Valor: {preco} adicionado a atributo preco!");
+        } else
+        {
+            this.preco = 0;
+            Console.WriteLine($"Valor {preco} não é válido, preco recebeu 0!");
+        }       
+    }
+
+    public void AdicionaEstoque(int estoque)
+    {
+        if (estoque >= 0)
+        {
+           this.estoque = estoque;
+            Console.WriteLine($"Valor: {estoque} adicionado a atributo estoque!");
+        } else
+        {
+            this.estoque = 0;
+            Console.WriteLine($"Valor {estoque} não é válido, estoque recebeu 0!");
+        }
+    }
+}
---
+++ produtos-classe/Program.cs
@@ -0,0 +1,18 @@
+﻿Produto prod1 = new Produto();
+
+prod1.nome = "Bola de Basquete";
+prod1.marca = "Penalty";
+prod1.AdicionaPreco(120);
+prod1.AdicionaEstoque(2);
+
+Console.WriteLine(prod1.DetalhesProduto);
+
+Produto prod2 = new Produto();
+
+prod2.nome = "Tênis";
+prod2.marca = "Nike";
+prod2.AdicionaPreco(245);
+prod2.AdicionaEstoque(-1);
+
+Console.WriteLine(prod2.DetalhesProduto);
+
