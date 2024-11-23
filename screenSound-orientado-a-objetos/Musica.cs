3fceaa5a6e5ee00bc65ea0884d0f75a096a92b50
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-23 19:33:33 -0300

    Contedos da aula 4, C# orientado-a-objetos

--- screenSound-orientado-a-objetos/Musica.cs
+++ screenSound-orientado-a-objetos/Musica.cs
@@ -1,24 +1,7 @@
-﻿class Musica
+class Musica
 {
-    public string Nome { get; set; }
-    public string Artista { get; set; }
-    public int Duracao { get; set; }
-    public bool Disponivel { get; set; }
-    public string Descricao => $"A música {Nome} pertence a banda {Artista}.";
-
-    public void ExibirInfosDaMusica()
-    {
-        Console.WriteLine($"Nome: {Nome}");
-        Console.WriteLine($"Artista: {Artista}");
-        Console.WriteLine($"Duração {Duracao} seg");
-        Console.WriteLine($"Descrição: {Descricao}");
-        if(Disponivel)
-        {
-            Console.WriteLine("Ouça agora!");
-        } else
-        {
-            Console.WriteLine("Assine o plano Plus+");
-        }
-        Console.WriteLine("--------------------------------");
-    }
+    public string nome;
+    public DateTime dataLancamento;
+    public int duracao;
+    public bool disponivel;
 }
--- screenSound-orientado-a-objetos/Program.cs
+++ screenSound-orientado-a-objetos/Program.cs
@@ -1,34 +1,4 @@
-﻿//Musica musica1 = new Musica();
-//musica1.Nome = "Faroeste Caboclo";
-//musica1.Artista = "Legião Urbana";
-//musica1.Duracao = (9 * 60);
-//musica1.Disponivel = true;
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.dataLancamento = DateTime.Parse("12/12/2008");
 
-//musica1.ExibirInfosDaMusica();
-
-//Musica musica2 = new Musica();
-//musica2.Nome = "Sweet child on mine";
-//musica2.Artista = "Guns and Roses";
-//musica2.Duracao = 303; 
-//musica2.Disponivel = false;
-
-//musica2.ExibirInfosDaMusica();
-
-Musica faroesteCaboclo = new Musica();
-faroesteCaboclo.Artista = "Legião Urbana";
-faroesteCaboclo.Duracao = (9 * 60);
-faroesteCaboclo.Disponivel = true;
-faroesteCaboclo.Nome = "Faroeste Caboclo";
-
-Musica eduardoEMonica = new Musica();
-eduardoEMonica.Artista = "Legião Urbana";
-eduardoEMonica.Duracao = (231);
-eduardoEMonica.Disponivel = true;
-eduardoEMonica.Nome = "Eduardo e Mônica";
-
-Album albumDoLegiaoUrbana = new Album();
-albumDoLegiaoUrbana.AdicionarMusica(faroesteCaboclo);
-albumDoLegiaoUrbana.AdicionarMusica(eduardoEMonica);
-
-albumDoLegiaoUrbana.ListarMusicas();
-Thread.Sleep(5000);
