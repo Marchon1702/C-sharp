a962a7f6f51db06956d7070cb4744f4aea2952d8
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-26 20:13:05 -0300

    Concluindo desafio da aula 3 do curso Dominando Orientações a Objetos.

---
+++ CalcArea-e-perimetro/Models/Circulo.cs
@@ -0,0 +1,19 @@
+﻿namespace CalcArea_e_perimetro.Models
+{
+    internal class Circulo : FormaGeometrica
+    {
+        public override double CalculaArea(double raio, double lados)
+        {
+            if (lados > 0)
+            {
+                Console.WriteLine("Isso não é um circulo!");
+                return 0;
+            }
+            return 3.14 * (Math.Pow(raio, 2)); 
+        }
+        public override double CalculaPerimetro(double raio)
+        {
+            return 2 * 3.14 * raio;
+        }
+    }
+}
---
+++ CalcArea-e-perimetro/Program.cs
@@ -0,0 +1,13 @@
+﻿// Criar uma hierarquia de classes representando formas geométricas, como Quadrado, Círculo e Triângulo. Utilize herança para criar uma classe base chamada FormaGeometrica, que contenha métodos para calcular a área e o perímetro de uma forma.
+
+using CalcArea_e_perimetro.Models;
+
+Console.WriteLine("Area e Perímetro de: ");
+FormaGeometrica quadrado = new Quadrado();
+Console.WriteLine($"Quadrado Área = {quadrado.CalculaArea(5, 5)} Perímetro = {quadrado.CalculaPerimetro(4)}");
+
+FormaGeometrica triangulo = new Triangulo();
+Console.WriteLine($"Triângulo Área = {triangulo.CalculaArea(8, 16)} Perímetro = {triangulo.CalculaPerimetro(3)}");
+
+FormaGeometrica circulo = new Circulo();
+Console.WriteLine($"Circulo Área = {circulo.CalculaArea(60, 0)} Perímetro = {circulo.CalculaPerimetro(60)}");
