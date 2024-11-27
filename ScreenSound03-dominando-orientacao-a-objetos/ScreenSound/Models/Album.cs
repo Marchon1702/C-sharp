9b75583d7f4df3bfaa2a2d7602acd539328c204d
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-27 20:10:39 -0300

    Conteúdos da aula 4 do cruso Dominando OrientaçÃo a Objetos.

--- ScreenSound03-dominando-orientacao-a-objetos/ScreenSound/Models/Album.cs
+++ ScreenSound03-dominando-orientacao-a-objetos/ScreenSound/Models/Album.cs
@@ -1,8 +1,11 @@
-﻿namespace ScreenSound.Models;
+﻿using ScreenSound.Interfaces;
 
-internal class Album
+namespace ScreenSound.Models;
+
+internal class Album : IAvaliavel
 {
-    private List<Musica> musicas = new List<Musica>();
+    private List<Musica> musicas = new();
+    private List<Avaliacao> notas = new();
 
     public Album(string nome)
     {
@@ -11,13 +14,21 @@ internal class Album
 
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
-    public List<Musica> Musicas => musicas;
+    public IEnumerable<Musica> Musicas => musicas;
+
+    public double Media { get {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);
+        } 
+    }
 
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
     }
 
+    public void AdicionarNota(Avaliacao nota) => notas.Add(nota);
+    
     public void ExibirMusicasDoAlbum()
     {
         Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
