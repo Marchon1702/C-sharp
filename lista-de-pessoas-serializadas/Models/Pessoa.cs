af60c71cfbbf7c09101ea2a6a26cfd0f8b5d3d4e
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-12-08 17:13:54 -0300

    Atividades concluidas, Criando arquivos Json, recuperando aqeuivos json, serializando e deserializando.

---
+++ lista-de-pessoas-serializadas/Models/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json;
+
+namespace lista_de_pessoas_serializadas.Models;
+
+internal class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Email { get; set; }
+
+    public static void SerializarRegistros(List<Pessoa> pessoas)
+    {
+        string json = JsonSerializer.Serialize(pessoas);
+        Console.WriteLine(pessoas);
+
+        string fileName = "lista-de-pessoas.json";
+
+        File.WriteAllText(fileName, json);
+        Console.WriteLine($"Json salvo em: {Path.GetFullPath(fileName)}");
+    }
+
+    public static List<Pessoa> DeserializaRegistros(string filename)
+    {  
+        if(File.Exists(filename))
+        {
+            string jsonString = File.ReadAllText(filename);
+            List<Pessoa> listaDePessoas = JsonSerializer.Deserialize<List<Pessoa>>(jsonString)!;
+            
+            foreach(Pessoa pessoa in listaDePessoas)
+            {
+                Console.WriteLine(pessoa.Nome);
+                Console.WriteLine(pessoa.Idade);
+                Console.WriteLine(pessoa.Email);
+                Console.WriteLine("-----------------------------");
+            }
+            return listaDePessoas;
+        } else
+        {
+            Console.WriteLine("Arquivo não encontrado!");
+            return new List<Pessoa>();
+        }
+    }
+
+    public static void FiltrarPorIdade(List<Pessoa> pessoas, int idade)
+    {
+        List<Pessoa> pessoasPorIdade = pessoas.Where(pessoa => pessoa.Idade == idade).ToList();
+        if(pessoasPorIdade.Count > 0)
+        {
+            foreach(Pessoa pessoa in pessoasPorIdade)
+            {
+                Console.WriteLine(pessoa.Nome);
+                Console.WriteLine(pessoa.Idade);
+                Console.WriteLine(pessoa.Email);
+                Console.WriteLine("-----------------------------");
+            }
+        } else
+        {
+            Console.WriteLine("Essa lista não possui pessoas da idade solicitada!");
+        }
+    }
+}
+
+
---
+++ lista-de-pessoas-serializadas/Program.cs
@@ -0,0 +1,50 @@
+﻿//Criar um programa que permite ao usuário inserir informações de várias pessoas, armazena essas informações em uma lista, serializa a lista em formato JSON e salva em um arquivo.
+
+//Criar um programa que lê um arquivo JSON contendo informações de várias pessoas, desserializa essas informações em uma lista e exibe na tela.
+
+//Criar um programa que lê um arquivo JSON contendo informações de várias pessoas, permite ao usuário inserir uma idade e exibe as pessoas com aquela idade.
+
+using lista_de_pessoas_serializadas.Models;
+
+//List<Pessoa> pessoas = new();
+
+//bool finalizado = false;
+//do
+//{
+//    Console.WriteLine("Registre-se...");
+//    Console.Write("Nome: ");
+//    string nome = Console.ReadLine();
+//    Console.Write("Idade: ");
+//    int idade = int.Parse(Console.ReadLine());
+//    if(idade < 1)
+//    {
+//        Console.WriteLine("Idade Inexistente...");
+//        return;
+//    }
+//    Console.Write("Email: ");
+//    string email = Console.ReadLine();
+//    if (!email.Contains("@"))
+//    {
+//        Console.WriteLine("Insira um email válido!");
+//        return;
+//    }
+
+//    pessoas.Add(new Pessoa
+//    {
+//        Nome = nome,
+//        Idade = idade,
+//        Email = email
+//    });
+
+//    Console.WriteLine("Finalizar registros [S]       [N] Continuar Registrando");
+//    string fim = Console.ReadLine();
+//    if(fim == "S" || fim == "s") finalizado = true;
+//} while (finalizado == false);
+
+//Pessoa.SerializarRegistros(pessoas);
+
+List<Pessoa> listaDePessoas = Pessoa.DeserializaRegistros("lista-de-pessoas.json");
+Thread.Sleep(3000);
+Console.Clear();
+Console.WriteLine("Filtrados por idade");
+Pessoa.FiltrarPorIdade(listaDePessoas, 12);
