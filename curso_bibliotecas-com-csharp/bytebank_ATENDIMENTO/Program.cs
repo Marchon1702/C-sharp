f1ef50277326eefafa3d3ebcf4a27238b1e91663
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-12-17 20:29:39 -0300

    Conteúdos do curso CSharp, trabalhando com bibliotecas.

---
+++ curso_bibliotecas-com-csharp/bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,13 @@
+﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+using gerador_pix;
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+//new ByteBankAtendimento().AtendimentoCliente();
+
+
+Console.WriteLine(GeradorPix.GerarChavePix());
+
+var listaDeChaves = GeradorPix.GerarListaDeChavesPix(5);
+foreach(var chave in listaDeChaves)
+{
+    Console.WriteLine(chave);
+}
---
+++ gerador-pix/GeradorPix.cs
@@ -0,0 +1,34 @@
+﻿namespace gerador_pix;
+
+/// <summary>
+/// Classe que gera chaves Pix usando o formato Guid.
+/// </summary>
+public static class GeradorPix
+{
+    /// <summary>
+    /// Método que retorna uma chave aleatória de PIX.
+    /// </summary>
+    /// <returns>Retorna uma chave PIX no formato String.</returns>
+    public static string GerarChavePix() => Guid.NewGuid().ToString();
+
+    /// <summary>
+    ///  Método que retorna uma lista aleatória de
+    ///  chaves Pix.
+    /// </summary>
+    /// <param name="quantidadeDeChaves"> Quantidade de chaves
+    /// a serem geradas.</param>
+    /// <returns>Lista de strings de chaves Pix.</returns>
+    public static List<string> GerarListaDeChavesPix(int quantidadeDeChaves)
+    {
+        if (quantidadeDeChaves <= 0) return null;
+        else
+        {
+            var chaves = new List<string>();
+            for (int i = 0; i < quantidadeDeChaves; i++)
+            {
+                chaves.Add(Guid.NewGuid().ToString());
+            }
+            return chaves;
+        }
+    }
+}
