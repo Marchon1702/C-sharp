98a02146d08373b0be1867a59fdbdf11fbd4ad40
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-12-18 21:47:36 -0300

    Conteúdos do curso C# Persistindo dados com Entity Framework Core.

---
+++ curso-persistencia-de-dados/ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco;
+
+internal class ArtistaDAL
+{
+    public IEnumerable<Artista> Listar()
+    {
+        var listaDeArtistas = new List<Artista>();
+        // Abrindo conexão
+        using var connection = new Connection().ObterConexao();
+        connection.Open();
+
+        // Definindo comando a ser executado no banco com a classe SqlCommand
+        string sql = "SELECT * FROM Artistas";
+        // Recebe o comando e conexão como argumento        
+        SqlCommand command = new SqlCommand(sql, connection);
+        // Criando um leitor de dados resultante do comando adicionado
+        using SqlDataReader dataReader = command.ExecuteReader();
+
+        // Enquanto tiver linhas da tabela para ler...
+        while (dataReader.Read())
+        {
+            // Pegando os dados das colunas nas linhas da tabela do indice atual
+            string nomeArtista = Convert.ToString(dataReader["Nome"])!;
+            string bioArtista = Convert.ToString(dataReader["Bio"])!;
+            int idArtista = Convert.ToInt32(dataReader["Id"]);
+
+            // Criando novo objeto Artista com os dados obtidos e adicionando à lista
+            Artista artista = new Artista(nomeArtista, bioArtista);
+            artista.Id = idArtista;
+
+            listaDeArtistas.Add(artista);
+        }
+
+        return listaDeArtistas;
+    }
+
+    // Adicionando artista recebi na Aplicação para o banco de dados...
+    public void Adicionar(Artista artista)
+    {
+        using var connection = new Connection().ObterConexao();
+        connection.Open();
+
+        string sql = "INSERT INTO Artistas (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)"; 
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@nome", artista.Nome);
+        command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+        command.Parameters.AddWithValue("@bio", artista.Bio);
+
+        // Verificando se a adição foi bem sucedida
+
+        // Método que verifica quantas linha foram afetadas
+        int linhasAfetadas = command.ExecuteNonQuery();
+        Console.WriteLine($"Linhas afetadas {linhasAfetadas}");
+    }
+
+    public void Editar(Artista artista)
+    {
+        using var connection = new Connection().ObterConexao(); 
+        connection.Open();
+
+        string sql = "UPDATE Artistas SET Nome = @nome, Bio = @bio WHERE Id = @id";
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@nome", artista.Nome);
+        command.Parameters.AddWithValue("@bio", artista.Bio);
+        command.Parameters.AddWithValue("@id", artista.Id);
+
+        int linhasAfetadas = command.ExecuteNonQuery();
+        Console.WriteLine($"Linhas afetadas {linhasAfetadas}");
+    }
+
+    public void Deletar(Artista artista)
+    {
+        using var connection = new Connection().ObterConexao();
+        connection.Open();
+
+        string sql = "DELETE FROM Artistas WHERE Id = @id";
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@id", artista.Id);
+
+        int linhasAfetadas = command.ExecuteNonQuery();
+        Console.WriteLine($"Linhas afetadas {linhasAfetadas}");
+    }
+}
---
+++ curso-persistencia-de-dados/ScreenSound/Program.cs
@@ -0,0 +1,97 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+//try
+//{
+//    // Intânciando conexão e chamando o método de uma vez...
+//    // Using descarta a variável connection ao final do escopo, isso serve para que a conexão não permaneça aberta permanentemente...
+//    using var connection = new Connection().ObterConexao();
+//    // Abrindo conexão...
+//    connection.Open();
+//    // Mostrando estado da conexão...
+//    Console.WriteLine(connection.State);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+try
+{
+    // A conexão já foi aberta no Método listar então não precisa ser aberta nesse escopo
+    // Instanciando a classe Connection para ter acesso o método listar
+    var artistaDAL = new ArtistaDAL();
+
+    // Adicionar adiciona um artista ao banco de dados...
+    //artistaDAL.Adicionar(new Artista("Jonas Brothers", "São irmãos"));
+    artistaDAL.Editar(new Artista("Michael Jackson", "Rei do Pop") { Id = 6});
+
+    // Listar retorna uma Lista de Artista instanciados em sua classe...
+    var listaDeArtistas = artistaDAL.Listar();
+    foreach (var artista in listaDeArtistas)
+    {
+        Console.WriteLine(artista);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+return;
+
+Artista ira = new Artista("Ira!", "Banda Ira!");
+Artista beatles = new("The Beatles", "Banda The Beatles");
+
+Dictionary<string, Artista> artistasRegistrados = new();
+artistasRegistrados.Add(ira.Nome, ira);
+artistasRegistrados.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> opcoes = new();
+opcoes.Add(1, new MenuRegistrarArtista());
+opcoes.Add(2, new MenuRegistrarMusica());
+opcoes.Add(3, new MenuMostrarArtistas());
+opcoes.Add(4, new MenuMostrarMusicas());
+opcoes.Add(-1, new MenuSair());
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine("Boas vindas ao Screen Sound 3.0!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar um artista");
+    Console.WriteLine("Digite 2 para registrar a música de um artista");
+    Console.WriteLine("Digite 3 para mostrar todos os artistas");
+    Console.WriteLine("Digite 4 para exibir todas as músicas de um artista");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
+    {
+        Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(artistasRegistrados);
+        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    } 
+    else
+    {
+        Console.WriteLine("Opção inválida");
+    }
+}
+
+ExibirOpcoesDoMenu();
